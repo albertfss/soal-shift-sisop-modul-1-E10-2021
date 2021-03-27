f55e42d9917242854ef8cf3d329434be94a68f8c
ALuthfiH <56031025+ALuthfiH@users.noreply.github.com>
2021-03-27 20:08:26 +0700

    Update soal1.sh
    Masih belum work, sedang dibenahi

--- soal1/soal1.sh
+++ soal1/soal1.sh
@@ -1,1 +1,32 @@
 #!/bin/bash
+
+#1.a
+ERROR=$(grep -E "ERROR" syslog.log)
+INFO=$(grep -E "INFO" syslog.log)
+
+error_count=$(grep -c "ERROR" syslog.log)
+info_count=$(grep -c "INFO" syslog.log)
+
+#1.b
+error_list=$(echo "$ERROR" | grep -Po "(?<=ERROR )(.*)(?=\()" | sort | uniq -c | sort -nr)
+
+#1.c
+user_list=$(grep -Po "(?<=\()(.*)(?=\))" syslog.log | sort | uniq)
+
+#1.d
+echo "Error,Count" > error_message.csv
+echo "$error_list" | while read baris
+do 
+	err=$(echo $baris | cut -d ' ' -f 2-)
+	errCount=$(echo $baris | cut -d ' ' -f 1)
+	echo "$err,$errCount" >> error_message.csv
+done
+
+#1.e
+echo "username,INFO,ERROR" > user_statistic.csv
+for i in $user_list
+do 
+	errorCount=$(echo grep "ERROR" syslog.log | grep -c $i)
+	infoCount=$(echo grep "INFO" syslog.log | grep -c $i)
+	echo "$i,$infoCount,$errorCount" >> user_statistic.csv
+done
